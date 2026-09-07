dcea59919af9244df2b34761393218af77d8cca9
Younesse Mbaye <younessembaye@MacBookAir.ht.home>
2026-09-06 23:54:38 -0500

    Added index.html and script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// A basic JavaScript "Hello World" example
 console.log("Hello, World!");
 
-document.querySelector("h1").textContent = "Hello, World! (updated by JavaScript)";
+document.querySelector("h1").textContent = "Hello, World!";
